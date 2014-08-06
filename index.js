cea6d8a67485b8f7e630b1aa2ad006f18c0b815e
francis.barreto <francis@lenddo.com>
2014-08-06 09:57:11 +0800

    added configuration to validate config object, modified the config loader to look into places where the ringcaptcha.js should be placed.

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 'use strict';
 
 var	path 		= require('path'),
+	fs          = require('fs'),
 	RingCaptcha = require('./lib/RingCaptcha.js'),
-	config 		= {},
-	DEFAULT_CONFIG_FNAME = "ringcaptcha.js";
+	Config      = require('./lib/Config.js').Config,
+	DEFAULT_CONFIG_FNAME = "ringcaptcha.js",
+	exists       = fs.existsSync || path.existsSync,
+	CONFIG_LOCATIONS = [
+		path.join(__dirname, '..', '..'), // above node_modules
+		process.cwd()
+	];
 
+//find the configuration file in the config locations defined.
+function getConfigFile() {
+	var filepath;
+
+	for(var i = 0; i < CONFIG_LOCATIONS.length; i++) {
+		filepath = path.join(path.resolve(CONFIG_LOCATIONS[i]), DEFAULT_CONFIG_FNAME);
+		if(!exists(filepath) ) continue;
+		return require(filepath).config;
+	}
+}
+var configError = 'CONFIG: From node_modules/ringcaptcha-node, copy ' + DEFAULT_CONFIG_FNAME + ' into the root directory of your app.';
 try{
-	config = require(path.join(__dirname, '..', '..', DEFAULT_CONFIG_FNAME) ).config;
+	var config = getConfigFile();
+	if(!config) {
+		throw new Error(configError);
+	}
 }catch(e) {
-	throw new Error('CONFIG: From node_modules/ringcaptcha-node, copy ' + DEFAULT_CONFIG_FNAME + ' into the root directory of your app.');
+	throw new Error(configError);
 }
 
-console.log(config.api);
-
-var RC = new RingCaptcha(config.api);
+var RC = new RingCaptcha( new Config(config.api) );
--- ringcaptcha.js
+++ ringcaptcha.js
@@ -1,8 +1,14 @@
-
+/**
+ * Configuration for api keys used to communicate to RingCaptcha API
+ * Please this file either in this node_module/ringcaptcha-node directory 
+ * or in your root app directory just two steps back from the node_modules directory.
+ * 
+ * { api_key: ****, app_key: ****, secret_key: **** }
+ */
 module.exports.config = {
 	api: {
-		api_key: "",
-		app_key: "",
-		secret_key: ""
+		api_key: "123",
+		app_key: "123",
+		secret_key: "123"
 	}
 };
